38bf6ebd60301f45c5897e2c5d32bccceb2cbe00
AxelJ26 <axelleyva26@gmail.com>
2024-05-13 11:51:03 -0600

    correcion de formulario

--- src/controllers/ExtintorController.php
+++ src/controllers/ExtintorController.php
@@ -3,19 +3,29 @@
 namespace Almacen\Ssst\controllers;
 
 use Flight;
+
+use Almacen\Ssst\dbrepo\Factory;
 use Almacen\Ssst\dbrepo\RepoMain;
 use Almacen\Ssst\utils\ValMRiesgos;
 use DateTime;
 use Exception;
 
 class ExtintorController extends Flight{
+
+    private  $repoRiesgos;
     public function __construct()
     {        
-       
-    
+        
+        $this->repoRiesgos = new Factory();
+        $this->repoRiesgos->getCatalogos(new RepoMain());
     }
 
     public function index (){
-        parent::render('Extintores/index');
+
+      
+       $this->repoRiesgos->getCat->table = 'cat_areas';
+       //$this->repoRiesgos->getCat->logic = "and";
+       $areas = $this->repoRiesgos->getCat->getAll();
+        parent::render('Extintores/index',['areas'=>$areas,'error'=>""]);
     }
 }
--- src/public/views/Extintores/index.php
+++ src/public/views/Extintores/index.php
@@ -3,19 +3,19 @@ require 'public/views/components/Header.php';
 require 'public/views/components/Nav.php';
 
 ?>
-<div class="d-flex flex-column"style="width: 50rem;">
+<div class="d-flex flex-column" >
 <div class="pt-2 mx-4 " style="background-color: #eceff4;">
 
-<div class="w-100 bg-light shadow-lg  rounded p-4 mt-3">
+<div class="w-100 bg-light shadow-lg rounded p-4 mt-3">
 
 
-<div class="row  mt-4 ms-2 me-2">
+<div class="row mt-4 ms-2 me-2">
 
-  <div class="col-md-6   ">
-  <h3 class="mb-6">Situaciones de riesgo</h3> 
+  <div class="col-6">
+  <h3 class="mb-6">Extintores</h3> 
   </div>
 
-  <div class="col-md-6   d-flex justify-content-end ">
+  <div class="col-6 d-flex justify-content-end">
   <button class="btn btn-danger" onclick="clearForm()">
   <i class="bi bi-arrow-repeat"></i>
   Limpiar</button>
@@ -25,58 +25,42 @@ require 'public/views/components/Nav.php';
 
 
 <div class="card-body">
-  
+</br>
   <form class="row g-3">
 
-    <div class="col-md-3" class="container mt-5">
-      
-      <div class="form-group">
-        <label for="monthpicker">Mes</label>
-        <input type="text" class="form-control monthpicker" id="monthpicker" placeholder="MM">
-      </div>    
-    </div>
 
-    <script>
-      // Inicializar el datepicker con la opción para mostrar solo meses
-      $( function() {
-        $(".monthpicker").datepicker({
-          dateFormat: 'MM', // Formato de fecha (solo el nombre del mes)
-          changeMonth: true,
-          changeYear: false,
-          showButtonPanel: true,
-          onClose: function(dateText, inst) {
-            var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-            $(this).val($.datepicker.formatDate('MM', new Date(0, month, 1)));
-          }
-        });
-      });
-    </script>
+
 
     <div class="row">
 
-      <div class="col-md-6">
+      <div class="col-6">
         <label for="formGroupExampleInput" class="form-label">Area</label>
-        <select class="form-select" aria-label="Default select example">
-          <option selected>area</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
+
+        <select class="form-select" id="area">
+          <option value="0" selected>Selecciona un área</option>
+          <?php
+          $opt = "";
+          foreach ($areas as $item) {
+            $opt .= "<option value='{$item["id_area"]}'>{$item["textArea"]}</option>";
+          }
+          echo $opt;
+          ?>
         </select>
       </div>
 
-      <div class="col-md-6">
+      <div class="col-6" >
         <label for="formGroupExampleInput" class="form-label">Nu. De inventario</label>
         <input type="text" class="form-control" id="txtNumIn" placeholder="Nu.De inventario">
       </div>
 
     </div>
 
-    <div class="row"> 
+    <div class="row mx-auto" style="width: 30rem; "> 
 
-      <div class="col-md-6 text-center">
+    <div class="col-12 text-center" >
       </br>
       </br>
-        <label for="pregunta1" class="form-label">El extintor está en el lugar designado</label>
+        <label for="pregunta1" class="form-label">1.-El extintor está en el lugar designado</label>
       </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -89,7 +73,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br>
-        <label for="pregunta2" class="form-label">El acceso al extintor está obstruido  </label>
+        <label for="pregunta2" class="form-label">2.-El acceso al extintor está obstruido  </label>
         </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -102,7 +86,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio3">No</label>
         </div>
         </br>
-        <label for="pregunta3" class="form-label">El señalamiento del extintor está obstruido</label>
+        <label for="pregunta3" class="form-label">3.-El señalamiento del extintor está obstruido</label>
         </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -115,7 +99,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br>
-        <label for="pregunta4" class="form-label">Las instruciones de operación sobre la placa del extintor son legibles</label>
+        <label for="pregunta4" class="form-label">4.-Las instruciones de operación sobre la placa del extintor son legibles</label>
         </br>
         <div class="form-check form-check-inline me-5 ms-5">
           <input class="form-check-input" type="radio" name="pregunta4" id="inlineRadio1" value="option1">
@@ -127,7 +111,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br>
-        <label for="pregunta5" class="form-label">Las manijas, boquilla y manguera están en buen estado </label>
+        <label for="pregunta5" class="form-label">5.-Las manijas, boquilla y manguera están en buen estado </label>
         </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -141,14 +125,8 @@ require 'public/views/components/Nav.php';
         </div>
 
         </br>
-      </div>
-
-
-      <div class="col-md-6 text-center">
-      </br>
-      </br>
-        <label for="pregunta6" class="form-label">Los sellos de inviolabilidad están en buenas codiciones</labe>
-      </br>
+        <label for="pregunta6" class="form-label">6.-Los sellos de inviolabilidad están en buenas codiciones</labe>
+        </br>
         <div class="form-check form-check-inline me-5 ms-5">
           <input class="form-check-input" type="radio" name="pregunta6" id="inlineRadio1" value="option1">
           <label class="form-check-label " for="inlineRadio1">Si</label>
@@ -159,7 +137,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br>
-        <label for="pregunta7" class="form-label">Las lecturas del manómetro están en el rango del operable. Cuando se trate de extintores sin manómetros, se debe determinar por peso si la carga es adecuada    </label>
+        <label for="pregunta7" class="form-label">7.-Las lecturas del manómetro están en el rango del operable. Cuando se trate de extintores sin manómetros, se debe determinar por peso si la carga es adecuada    </label>
         </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -172,7 +150,7 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br>
-        <label for="pregunta8" class="form-label">Se observa cualquier envidencia de daños físico, como corrosíon, escape de presíon u obtrucción </label>
+        <label for="pregunta8" class="form-label">8.-Se observa cualquier envidencia de daños físico, como corrosíon, escape de presíon u obtrucción </label>
         </br>
 
         <div class="form-check form-check-inline me-5 ms-5">
@@ -185,88 +163,33 @@ require 'public/views/components/Nav.php';
           <label class="form-check-label" for="inlineRadio2">No</label>
         </div>
         </br> 
-      </div>
 
-      <div class="row">
-        <div class="col-md-6">
-          <label for="formGroupExampleInput" class="form-label">Altura del extintor</label>
-            <input type="text" class="form-control" id="txtNumIn" placeholder="Altura">
+        
+      </div>
+        </div>
+        </br> 
+        
+
+        <div class="row">
+        <div class="col-6">
+          
             </br>
             <label for="formGroupExampleInput" class="form-label">Peso del extintor</label>
             <input type="text" class="form-control" id="txtNumIn" placeholder="Altura">
         </div>
 
-        <div class="col-md-6">
-          <label for="formGroupExampleInput" class="form-label">Tipo de polvo</label>
-          <select class="form-select" aria-label="Default select example">
-            <option selected>area</option>
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-          </select>
-        </div>
+        <div class="col-6">
+        </br>
+        <label for="formGroupExampleInput" class="form-label">Altura del extintor</label>
+            <input type="text" class="form-control" id="txtNumIn" placeholder="Altura">
       </div>
-
-      <div class="row mt-3">
-        <div class="col-md-6" class="container mt-5">
-          <div class="form-group">
-            <label for="datepicker2">Ultima recarga</label>
-            <input type="text" class="form-control datepicker" id="datepicker2" placeholder="DD/MM/YY">
-          </div>  
-        </div>
-
-        <div class="col-md-6" class="container mt-5">
-          <div class="form-group">
-            <label for="datepicker3">Proxima recarga</label>
-            <input type="text" class="form-control datepicker" id="datepicker3" placeholder="DD/MM/YY">
-          </div>  
-        </div>
       </div>
-
-      <script>
-      $( function() {
-        $(".datepicker").datepicker({
-          dateFormat: 'yy-mm-dd', // Formato de fecha
-          onSelect: function(dateText) {
-            // Mostrar la fecha seleccionada en el campo de texto
-            $(this).val(dateText);
-          }
-        });
-      });
-      </script>
-
 
       <div class="col-12 mt-2 ms-2 me-2">
         <button type="submit" class="btn btn-success col-12">Guardar</button>
       </div>
-    </div>
-</form>
-</div>
+  </form>
+      </div>
 
-</div>
-</div>
-
-
-<style>
-  tr{
-    cursor: pointer;
-  }
-  .ids{
-    width: 4rem;
-  }
-  .descri{
-    width: 40%;
-     }
-  .fechaRe{
-    width: 6rem;
-  }
-  .prioridad{
-width: 6rem;
-    }
-  .estatus{
-    width: 9rem;
-}
-</style>
-
-<?php require 'public/views/components/Footer.php'; ?>
+      <?php require 'public/views/components/Footer.php'; ?>
 
