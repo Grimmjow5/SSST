56cbcca836feb4d0d4260daf933c7bfcefdfdb2f
sebastian <poscordero@gmail.com>
2024-05-28 12:57:17 -0600

    Se quitaron CDN y se descargaron

--- src/public/views/Login.php
+++ src/public/views/Login.php
@@ -97,7 +97,7 @@
         </div>
     </section>
     <script src="/public/js/jquery.min.js"></script>
-    <script src="/public/js/popper.js"></script>
+    <script src="/public/js/popper.min.js"></script>
     <script src="/public/js/bootstrap.min.js"></script>
     <script src="/public/js/main.js"></script>
 
--- src/public/views/components/Footer.php
+++ src/public/views/components/Footer.php
@@ -8,15 +8,14 @@ a{
 
 }
 </style>
- <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.3/dist/js/bootstrap.bundle.min.js"></script>-->
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>    
+<script src="/public/js/popper.min.js" crossorigin="anonymous"></script>
+<script src="/public/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>    
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.0.5/b-3.0.2/b-html5-3.0.2/datatables.min.js"></script>
+<script src="/public/js/bootstrap.bundle.min.js"></script>
+<script src="/public/js/datatables.min.js"></script>
 
 
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <script src="/public/js/sweetalert.min.js"></script>
     <script>
         const btn = document.getElementById('toggle');
         const slide = document.getElementById('slide');
