77b5fe47dbf00f55591ad65553e5896e6f5922fa
sebastian <poscordero@gmail.com>
2024-05-30 12:57:19 -0600

    Solucion de rutas para dependecias de CSS

--- SSST/src/public/views/components/Header.php
+++ SSST/src/public/views/components/Header.php
@@ -3,7 +3,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
-<link href="./src/public/css/bootstrap.min.css" rel="stylesheet">
+<link href="src/public/css/bootstrap.min.css" rel="stylesheet">
 <link href="src/public/css/dataTables.dataTables.css" rel="stylesheet">
 <link href="src/public/css/datatables.min.css" rel="stylesheet">
 <link rel="stylesheet" href="src/public/css/buttons.dataTables.css">
