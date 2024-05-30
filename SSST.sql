70f1b9e908f94e5d63db83f4f8b30e6246b4d7ec
sebastian <poscordero@gmail.com>
2024-05-30 10:12:38 -0600

    Respaldo de DB

--- SSST.sql
+++ SSST.sql
@@ -2,7 +2,7 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
+-- Servidor: 127.0.0.1n
 -- Tiempo de generación: 30-05-2024 a las 17:55:22
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
@@ -11,14 +11,14 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
+CREATE DATABASE SSST;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `nueva`
+-- Base de datos: `SSST`
 --
 
 DELIMITER $$
